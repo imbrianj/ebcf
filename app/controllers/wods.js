7b5052a392ba2913bc6ac87fcdbb2a366e5763c3
Samantha Brender <sbrender@simplymeasured.com>
2016-01-07 08:36:58 -0800

    Added search for wods by date

--- app/controllers/wods.js
+++ app/controllers/wods.js
@@ -4,6 +4,9 @@ export default Ember.Controller.extend({
   didChange: Ember.observer('searchedTags', function(){
     this.send('searchInputChanged');
   }),
+  dateChanged: Ember.observer('wodDate', function(){
+    this.send('dateInputChanged', this.get('wodDate'));
+  }),
   actions: {
     searchInputChanged() {
       var tags = this.get('searchedTags');
@@ -15,6 +18,20 @@ export default Ember.Controller.extend({
           });
         });
         this.set('wods', wods);
+      } else {
+        this.set('wods', this.store.findAll('wod'));
+      }
+    },
+    dateInputChanged(date) {
+      if (date) {
+        var filteredWods = [];
+        this.get('wods').forEach( function(wod){
+          var wodDate = moment(wod.get('date')).format('LL')
+          if (wodDate === moment(date).format('LL')){
+            filteredWods.pushObject(wod);
+          }
+        });
+        this.set('wods', filteredWods);
       } else {
         this.set('wods', this.store.findAll('wod'));
       }
