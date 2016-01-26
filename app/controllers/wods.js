138ba2f2154e965bbc7bf39e20309450e6b7793c
Samantha Brender <sbrender@simplymeasured.com>
2016-01-26 14:25:55 -0800

    Bulk add tags to wods

--- app/controllers/wods.js
+++ app/controllers/wods.js
@@ -32,6 +32,27 @@ export default Ember.Controller.extend({
       } else {
         this.set('wods', this.store.findAll('wod'));
       }
+    },
+    bulkUpdateTags(){
+      var wods = this.get('wods');
+      var tags = this.get('tags');
+      wods.forEach( function(wod) {
+        var strength = wod.get('strength').toLowerCase();
+        var conditioning = wod.get('conditioning').toLowerCase();
+        tags.forEach(function(tag) {
+          if (strength.search(tag.get('value')) > 0) {
+            wod.get("tags").pushObject(tag);
+            tag.get("wods").pushObject(wod);
+            tag.save();
+            wod.save();
+          } else if (conditioning.search(tag.get('value')) > 0) {
+            wod.get("tags").pushObject(tag);
+            tag.get("wods").pushObject(wod);
+            tag.save();
+            wod.save();
+          }
+        });
+      });
     }
   }
 });
