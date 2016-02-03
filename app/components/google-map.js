95b5c2fd020b66a6b4e183d67b9b81668aecde4f
Samantha Brender <sbrender@simplymeasured.com>
2016-02-02 21:54:16 -0800

    Added map right way

---
+++ app/components/google-map.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  insertMap: function() {
+    var mapDiv = document.getElementById('map');
+
+
+    var myLatLng = {lat: 47.614265, lng: -122.345141};
+    var map = new google.maps.Map(mapDiv, {
+      center: myLatLng,
+      zoom: 15,
+      scrollwheel: false,
+      disableDefaultUI: true
+    });
+
+    var marker = new google.maps.Marker({
+     position: myLatLng,
+     map: map,
+     title: 'Elliott Bay Crossfit'
+   });
+ }.on('didInsertElement')
+});
