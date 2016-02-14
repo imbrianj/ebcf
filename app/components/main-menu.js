65fb5a91ea98fcbdb1d415069ccb5f92c1c503a6
Samantha Brender <sbrender@simplymeasured.com>
2016-02-14 14:53:59 -0800

    Animated menu working

---
+++ app/components/main-menu.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  initializeComponent: Ember.on('didInsertElement', function() {
+  // Ember.$('.main-menu').transition('fade down');
+  var lastScrollTop = $(window).scrollTop();
+  Ember.$(window).scroll( function(event) {
+
+     var st = Ember.$(this).scrollTop();
+
+     if (st > lastScrollTop) { //down
+       if (Ember.$('.main-menu').transition('is visible')) {
+         Ember.$('.main-menu').transition('hide');
+       }
+     } else { // up
+       if (!Ember.$('.main-menu').transition('is visible')){
+         Ember.$('.main-menu').transition('fade down');
+       }
+     }
+     lastScrollTop = st;
+
+  });
+})
+});
