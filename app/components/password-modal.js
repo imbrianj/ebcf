0b4168a3788018385d9c089d557d60eabf68ee1b
Samantha Brender <sbrender@simplymeasured.com>
2016-04-03 12:58:59 -0700

    Upgraded Ember to 2.4 and changed password modal

--- app/components/password-modal.js
+++ app/components/password-modal.js
@@ -3,6 +3,7 @@ import CryptoJS from 'npm:crypto-js';
 
 export default Ember.Component.extend({
   loginError: false,
+  ajax: Ember.inject.service(),
   showPasswordModa: function(){
     $('#password-modal').modal('setting', 'closable', false).modal('show');
   }.on('didInsertElement'),
@@ -11,23 +12,20 @@ export default Ember.Component.extend({
       var password = this.get('password');
       var self = this;
       var sha1 = CryptoJS.SHA1(password).toString();
-      var ajaxOptions = {
-        url: '/api/v1/verify-password',
+
+      this.get('ajax').request('/api/v1/verify-password', {
+        method: 'POST',
         data: {
           password: sha1
         },
-        type: 'POST',
-        dataType: 'json'
-      };
-      Ember.$.ajax(ajaxOptions).then(function(response){
+        dataType: "json"
+      }).then(function(response){
         if (response.success) {
-          // self.sendAction('loginSuccess');
           $("#password-modal").modal("hide");
         }	else {
           self.set('loginError', true);
         }
       });
-      return false;
     }
   }
 });
