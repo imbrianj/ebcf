4d0f3622940b83d7c0875e1c18e9daac8ce10567
Samantha Brender <sbrender@simplymeasured.com>
2016-05-06 22:44:02 -0700

    Updated styles for small phones

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -2,24 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   customOptions: {scrollwheel: false,
-                  disableDefaultUI: true}
- //  insertMap: function() {
- //    var mapDiv = document.getElementById('map');
- //
- //
- //    var myLatLng = {lat: 47.614265, lng: -122.345141};
- //    var map = new google.maps.Map(mapDiv, {
- //      center: myLatLng,
- //      zoom: 15,
- //      scrollwheel: false,
- //      disableDefaultUI: true
- //    });
- //
- //    var marker = new google.maps.Marker({
- //     position: myLatLng,
- //     map: map,
- //     title: 'Elliott Bay Crossfit'
- //   });
- // }.on('didInsertElement')
-
+                  disableDefaultUI: false}
 });
