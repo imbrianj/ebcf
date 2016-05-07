99fd30c546fe9c87ba2908159743b94439c6af01
Samantha Brender <sbrender@simplymeasured.com>
2016-05-07 14:21:30 -0700

    Added filter by date on admin page

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -3,33 +3,26 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   sortProps: ['date:desc'],
   sortedWods: Ember.computed.sort('wods', 'sortProps'),
-  dateDepth: 1,
-  _getWodsOlderThan(weeksAgo) {
-    return this.store.query('wod', {
-      filter: {
-        simple: {
-          publishDate: {
-            $gt: weeksAgo,
-            $lt: window.moment().toDate()
-          },
-          enabled: true
-        }
-      }
-    });
-  },
   actions: {
-    getOlder() {
+    filterDate() {
+      var fromDate = this.get('fromDate');
+      var toDate = this.get('toDate');
       var _this = this;
-      var dateDepth = this.get('dateDepth') + 1;
-      var weeksAgo = window.moment().day(-7 * dateDepth).toDate();
 
-      Ember.$('.older').addClass('loading');
+      Ember.$('.filter').addClass('loading');
 
-      this._getWodsOlderThan(weeksAgo).then(function(wods){
-        Ember.$('.older').removeClass('loading');
-
+      this.store.query('wod', {
+        filter: {
+          simple: {
+            publishDate: {
+              $gt: fromDate,
+              $lt: toDate
+            }
+          }
+        }
+      }).then(function(wods){
         _this.set('wods', wods);
-        _this.set('dateDepth', dateDepth);
+        Ember.$('.filter').removeClass('loading');
       });
     }
   }
