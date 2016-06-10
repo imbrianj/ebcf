50fe6da58d9130cd2a797e9362c6d726f09198cf
Samantha Brender <sbrender@simplymeasured.com>
2016-06-10 14:15:46 -0700

    Updated bios

--- app/routes/coaches.js
+++ app/routes/coaches.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   activate: function() {
     document.title = "Our Coaches";
-    Ember.$("meta[name=description]").attr("content", "We have three knowledgeable and experienced coaches who know how to drive results.");
+    Ember.$("meta[name=description]").attr("content", "We have knowledgeable and experienced coaches who know how to drive results.");
     Ember.$("meta[name=prerender-status-code]").attr("content", "200");
   },
 });
