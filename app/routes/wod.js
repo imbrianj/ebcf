f232fdcf07d3ae8ee63ac517e5fa9d42678eb39d
Samantha Brender <sbrender@simplymeasured.com>
2016-11-23 21:46:40 -0800

    Added ember-cli-meta-tags to wod page

--- app/routes/wod.js
+++ app/routes/wod.js
@@ -1,12 +1,11 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-  activate: function() {
-    document.title = "WOD";
-    Ember.$("meta[name=description]").attr("content", "EBCF Workout of the Day");
-    Ember.$("meta[name=prerender-status-code]").attr("content", "200");
-  },
+const {
+  get,
+  set,
+} = Ember;
 
+export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('wod', params.wod_id);
   },
@@ -14,5 +13,38 @@ export default Ember.Route.extend({
   beforeModel() {
     this.controllerFor('application').set('titleImage', 'wods');
     this.controllerFor('application').set('titleHeader', 'WOD ARCHIVE');
-  }
+  },
+
+  afterModel(model) {
+    this.setHeadTags(model);
+  },
+
+  setHeadTags(model) {
+    var headTags = [
+      {
+        type: 'meta',
+        tagId: 'meta-description-tag-wod',
+        attrs: {
+          name: 'title',
+          content: `Workout of the Day for ${get(model, 'prettyDate')}`,
+        }
+      }, {
+        type: 'meta',
+        tagId: 'meta-title-tag-wod',
+        attrs: {
+          name: 'description',
+          content: get(model, 'prettyDate'),
+        }
+      }, {
+        type: 'meta',
+        tagId: 'prerender-status-code-wod',
+        attrs: {
+          name: 'prerender-status-code',
+          content: '200',
+        }
+      }
+    ];
+
+    set(this, 'headTags', headTags);
+  },
 });
