daa70a15add767068aa8c0492aca2629156bdb6f
Samantha Brender <sbrender@simplymeasured.com>
2016-12-31 16:02:16 -0800

    Added alts to all wod pics

--- app/components/wod-item.js
+++ app/components/wod-item.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const {
+  computed,
+  get,
+  Component
+ } = Ember;
+
+export default Component.extend({
   classNames: ["wod-item"],
   placeHolderImage: "/assets/place-holder-image.png",
+  alt: computed('prettyDate', function() {
+    return `Workout of the Day for ${get(this, 'prettyDate')}`;
+  }),
 });
