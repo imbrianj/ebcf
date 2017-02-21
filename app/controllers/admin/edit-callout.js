01659fb342648557a9a677b6420b342218a09045
Samantha Brender <sbrender@simplymeasured.com>
2017-02-20 20:56:27 -0800

    lint for edit callout

--- app/controllers/admin/edit-callout.js
+++ app/controllers/admin/edit-callout.js
@@ -8,24 +8,23 @@ const {
 
 export default Controller.extend({
   actions: {
-    updateCallout(){
-      var _this = this;
-      var callout = get(this, 'callout');
+    updateCallout() {
+      let callout = get(this, 'callout');
 
-      var startDay = get(callout, 'startDay');
-      var startTime = get(callout, 'startTime');
-      var startDate = window.moment(startDay + " " + startTime).toDate();
+      let startDay = get(callout, 'startDay');
+      let startTime = get(callout, 'startTime');
+      let startDate = window.moment(`${startDay} ${startTime}`).toDate();
 
-      var endDay = get(callout, 'endDay');
-      var endTime = get(callout, 'endTime');
-      var endDate = window.moment(endDay + " " + endTime).toDate();
+      let endDay = get(callout, 'endDay');
+      let endTime = get(callout, 'endTime');
+      let endDate = window.moment(`${endDay} ${endTime}`).toDate();
 
       set(callout, 'startDate', startDate);
       set(callout, 'endDate', endDate);
 
-      callout.save().then(function() {
-        _this.transitionToRoute('admin.callouts');
+      callout.save().then(() => {
+        this.transitionToRoute('admin.callouts');
       });
-    }
-  }
+    },
+  },
 });
