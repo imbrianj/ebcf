0176f5a8e686b8da5c4c9a766a62d7e50f094c1f
Samantha Brender <sbrender@simplymeasured.com>
2017-02-20 20:55:36 -0800

    lint for cli build and env config

--- config/environment.js
+++ config/environment.js
@@ -1,24 +1,25 @@
 /* jshint node: true */
+'use strict';
 
 module.exports = function(environment) {
-  var ENV = {
+  let ENV = {
     modulePrefix: 'ebcf',
-    environment: environment,
+    environment,
     baseURL: '/',
     locationType: 'auto',
     contentSecurityPolicy: {
       'style-src': "'self' 'unsafe-inline' https://*.googleapis.com",
       'script-src': "'self' 'unsafe-eval' https://*.googleapis.com https://*.gstatic.com",
       'img-src': "'self' http://ebcftest.s3.amazonaws.com/* https://s3-us-west-2.amazonaws.com/ebcf/assets/* http://jsawebsitepublic.s3.amazonaws.com/wp-content/uploads/ https://*.googleapis.com https://*.gstatic.com",
-      'connect-src' : "'self' http://localhost:4500 http://ebcftest.s3.amazonaws.com/ https://ebcftest.s3-us-west-2.amazonaws.com https://ebcf-server.herokuapp.com/",
+      'connect-src': "'self' http://localhost:4500 http://ebcftest.s3.amazonaws.com/ https://ebcftest.s3-us-west-2.amazonaws.com https://ebcf-server.herokuapp.com/",
       'font-src': "'self' data: https://fonts.gstatic.com/s/ https://*.gstatic.com",
-      'media-src': "'self' https://s3-us-west-2.amazonaws.com/ebcftest/assets/ https://s3-us-west-2.amazonaws.com/ebcf/assets/"
+      'media-src': "'self' https://s3-us-west-2.amazonaws.com/ebcftest/assets/ https://s3-us-west-2.amazonaws.com/ebcf/assets/",
     },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      }
+      },
     },
 
     APP: {
@@ -26,7 +27,7 @@ module.exports = function(environment) {
       // when it is created
     },
     moment: {
-      includeTimezone: 'all'
+      includeTimezone: 'all',
     },
   };
 
@@ -34,7 +35,6 @@ module.exports = function(environment) {
     key: 'AIzaSyA5rpm0k1V1fHLU4izXgOs9H7wa2ZS7E7U',
     protocol: 'https',
     draggable: false,
-    // scrollwheel: false
   };
 
   if (environment === 'development') {
@@ -55,10 +55,6 @@ module.exports = function(environment) {
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
     ENV.APP.rootElement = '#ember-testing';
-  }
-
-  if (environment === 'production') {
-
   }
 
   return ENV;
