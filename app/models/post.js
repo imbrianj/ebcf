0981579c9e7cc32bc8c5cfc824b00f073be1498c
Samantha Brender <sbrender@simplymeasured.com>
2017-03-28 20:14:53 -0700

    Linting all the models

--- app/models/post.js
+++ app/models/post.js
@@ -3,14 +3,19 @@ import Ember from 'ember';
 
 const { computed } = Ember;
 
-export default DS.Model.extend({
-  enabled: DS.attr('boolean'),
-  title: DS.attr('string'),
-  date: DS.attr('date'),
-  publishDate: DS.attr('date'),
-  contentImage: DS.attr('string'),
-  bannerImage: DS.attr('string'),
-  content: DS.attr('string'),
+const {
+  attr,
+  Model,
+} = DS;
+
+export default Model.extend({
+  enabled: attr('boolean'),
+  title: attr('string'),
+  date: attr('date'),
+  publishDate: attr('date'),
+  contentImage: attr('string'),
+  bannerImage: attr('string'),
+  content: attr('string'),
   prettyDate: computed('date', function() {
     return window.moment(this.get('date')).utc().format('ddd MM.DD.YYYY').toUpperCase();
   }),
@@ -21,12 +26,12 @@ export default DS.Model.extend({
     return window.moment(this.get('publishDate')).startOf('day').format('YYYY-MM-DD');
   }),
   prettyPublishDate: computed('date', function() {
-    return window.moment(this.get('publishDate')).tz("America/Los_Angeles").format('ddd MM.DD.YYYY h:mm a').toUpperCase();
+    return window.moment(this.get('publishDate')).tz('America/Los_Angeles').format('ddd MM.DD.YYYY h:mm a').toUpperCase();
   }),
   publishTime: computed('publishDate', function() {
     return window.moment(this.get('publishDate')).format('HH:mm');
   }),
   active: computed('publishDate', 'enabled', function() {
     return (window.moment(this.get('publishDate')) < window.moment()) && this.get('enabled');
-  })
+  }),
 });
